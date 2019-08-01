443be9dd5e7cfd12069604e9cb0627fb4c992dde
Marcos Gutiérrez <marcos.gutierrez.hernandez@gmail.com>
2019-08-02 00:31:34 +0200

    se crea una nueva clase para runners (con sus atributos disfraz, posición y nombre) y con el método avanza. Se añade la competición (método avanza de runner y condición de fin) en Game. Y se modifica tamaño de smallball

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-import pygame
-import sys
+import pygame, sys
+import random
+
+class Runner():
+    def __init__(self, x=0, y=0):
+        self.custome = pygame.image.load("images/smallball.jpg")
+        self.position = [x, y]
+        self.name = "Bolita"
+    
+    def avanzar(self):
+        self.position[0] += random.randint(1, 6)
 
 class Game():
     
-    corredores = []
+    runners = []
+    __startLine = -10
+    __finishLine = 620
     
     def __init__(self):
         
@@ -11,30 +22,35 @@ class Game():
         pygame.display.set_caption("Carrera de bichos")
         self.background = pygame.image.load("images/background.jpg")
         
-        self.runner = pygame.image.load("images/smallball.jpg")
+        #firstRunner = Runner (self.__startLine,0)
+        #runners.append(firstRunner)
+        
+        self.runners.append(Runner(self.__startLine, 240))
+        self.runners[0].name = "dekachivu dekachivu dekachivaka"
     
     def competir(self):
         
-        x = 0
-        hayGanador = False
         
-        while not hayGanador:
+        gameOver = False
+        
+        while not gameOver:
             # comprobación de los eventos
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
+                    gameOver = True
                     pygame.quit()
                     sys.exit()
             
+            self.runners[0].avanzar()
+            if self.runners[0].position[0] >= self.__finishLine:
+                print("{} ha ganado".format(self.runners[0].name))
+                gameOver = True
+                
             # refrescar/renderizar la pantalla
             self.__screen.blit(self.background, (0,0))
-            self.__screen.blit(self.runner, (x, 240))
+            self.__screen.blit(self.runners[0].custome, self.runners[0].position)
             pygame.display.flip()
-            
-            x += 3
-            if x >= 250:
-                hayGanador = True
-
-   
+             
     
     
 if __name__ == '__main__':
